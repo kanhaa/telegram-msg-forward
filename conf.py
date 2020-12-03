1db1c95ae0187551888d129cacb7cd893d97bc56
kanhaa <75423572+kanhaa@users.noreply.github.com>
2020-12-03 17:03:43 +0530

    Update and rename sample-conf.py to conf.py

--- conf.py
+++ conf.py
@@ -9,9 +9,9 @@
 # There is no need to enter any URL. Just enter the App title and short name of your choice.
 # Click on Create application at the end.
 # Copy the API_ID and API_HASH values and paste here.
-API_ID = 1225
+API_ID = 1144902
 # NOTE: The API_HASH must be in single quotes. Below is a sample.
-API_HASH = 'd15e36f952b2d0c547d'
+API_HASH = 'e743e5a4f35076e4c558a4bd713082e9'
 
 
 # OWN_CHANNEL_ID is the unique id of your channel/group where you want to receive the updates.
@@ -19,10 +19,10 @@ API_HASH = 'd15e36f952b2d0c547d'
 #  After login, if you have'nt already. It would instruct to you to got to the channel you want the id for and send any message there.
 # After you send the message check your terminal. Find your message and copy the channel id to OWN_CHANNEL_ID.
 # Be careful with the minus(-) sign. If it is in the id then place it as well.
-OWN_CHANNEL_ID = -1003523231
+OWN_CHANNEL_ID = -1001268815373
 
 # Get the ids for all the channel you want to receive updates from and paste them here.
-CHANNELS_TO_GET_UPDATES_FROM = [-131451, -1475134, 4751754715, 45476152]
+CHANNELS_TO_GET_UPDATES_FROM = [-1001381869748]
 
 
 # You are done!
